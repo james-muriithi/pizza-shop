b8ce65a5d7fef30fc0dad096ccf9e3db91fce2e1
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-02-10 20:54:43 +0300

    Add the role class

--- app/models.py
+++ app/models.py
@@ -3,8 +3,16 @@ from . import db
 
 class User(db.Model):
     #...
+    '''
+    User tables
+    '''
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
- 
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+   
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -15,3 +23,15 @@ class User(db.Model):
 
     def verify_password(self,password):
         return check_password_hash(self.pass_secure,password)
+
+class Role(db.Model):
+    '''
+    Roles table
+    '''
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    role = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return f'Role {self.name}'
