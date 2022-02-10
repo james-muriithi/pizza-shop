9723d4456d52654bb566a29a161b418b1143aef1
james-muriithi <muriithijames556@gmail.com>
2022-02-10 13:42:46 +0300

    added order page

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-from .main import main as main_blueprint
-from .auth import auth as auth_blueprint
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -16,6 +15,8 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
     # register blueprint
+    from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
     app.register_blueprint(main_blueprint)
     app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,19 @@
-from flask import render_template
+from flask import render_template, abort
 from . import main
+from ..models import Pizza
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+# order
+@main.route('/order/<pizza_id>', methods=["GET", "POST"])
+def order(pizza_id):
+    '''Order pizza'''
+    pizza = Pizza.query.get(pizza_id)
+
+    if not pizza:
+        return abort(404)
+
+
+    return render_template('order.html', pizza=pizza)    
