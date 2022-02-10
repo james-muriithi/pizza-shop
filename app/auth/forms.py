67553892f21013d6d048b3b2ac4a3bf7028bcb1a
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-02-10 21:45:36 +0300

    Add the login and signup forms to be authenticated

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,18 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,ValidationError
-from wtforms.validators import Required,Email,EqualTo
+from wtforms.validators import DataRequired,Email,EqualTo
 from ..models import User
 
 class SignupForm(FlaskForm):
-    email = StringField('Your Email Address',validators=[Required(),Email()])
-    name = StringField('Enter your username',validators = [Required()])
-    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    email = StringField('Your Email Address',validators=[DataRequired(),Email()])
+    name = StringField('Enter your username',validators = [DataRequired()])
+    password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
     submit = SubmitField('Sign Up')
 
     def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
                 raise ValidationError('There is an account with that email')
 
-    def validate_username(self,data_field):
+    def validate_name(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
+
+
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,31 @@
 from crypt import methods
 from flask import render_template
 from . import auth
+from flask import render_template,redirect,url_for
+from ..models import User
+from .forms import SignupForm, LoginForm
+from .. import db
+
+@auth.route('/signup',methods = ["GET","POST"])
+def register():
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, name = form.name.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/signup.html',signup_form = form)
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def index():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/login.html',login_form = form)
 
-@auth.route('/signup', methods = ['GET', 'POST'])
-def signup():
-    return render_template('signup.html')    
+   
